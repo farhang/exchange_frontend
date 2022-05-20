ddaced2b90a576e5446babe51b9aa590cf10df25
Farhang Darzi <farhang.darzi@gmail.com>
2022-05-21 02:40:35 +0430

    chore(remove playwright webserver config): playwright

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,11 +1,5 @@
 import { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
-  webServer: {
-    command: 'npm run start',
-    url: 'http://localhost:3000/',
-    timeout: 120 * 1000,
-    reuseExistingServer: !process.env.CI,
-  },
   use: {
     baseURL: process.env.PLAYWRIGHT_TEST_BASE_URL || 'http://localhost:3000',
   },
