32a36dd9975834de6cb4efbe520112fbdb03b5f3
Farhang Darzi <farhang.darzi@gmail.com>
2022-05-21 02:05:55 +0430

    test: edit test and tests config

--- e2e/example.spec.ts
+++ e2e/example.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from '@playwright/test'
-import  config from '../playwright.config'
+import config from '../playwright.config'
 test('should navigate to the about page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   await page.goto('./')
