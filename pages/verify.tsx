7ff5d1fd7a053e67b62fa0cc4b49a3b9322ef058
Farhang Darzi <farhang.darzi@gmail.com>
2022-06-08 01:30:42 +0430

    fix(lint): fix all lint errors

--- pages/verify.tsx
+++ pages/verify.tsx
@@ -73,12 +73,12 @@ export default function Verify() {
   const {
     register,
     handleSubmit,
-    setValue,
     formState: { errors },
   } = useForm({
     resolver: yupResolver(schema),
     defaultValues: {
       code: null,
+      email: null,
     },
   })
   const onSubmit = (data: any) => {
@@ -112,7 +112,7 @@ export default function Verify() {
               <FormControl
                 display={'none'}
                 mb={4}
-                isInvalid={errors.email?.message}
+                isInvalid={!!errors.email?.message}
               >
                 <Input
                   disabled
@@ -122,7 +122,7 @@ export default function Verify() {
                 />
               </FormControl>
 
-              <FormControl mb={4} isInvalid={errors.code?.message}>
+              <FormControl mb={4} isInvalid={!!errors.code?.message}>
                 <FormLabel mb={1} htmlFor="code">
                   Verification code
                 </FormLabel>
